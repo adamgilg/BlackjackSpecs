8843dafb1eade6d35c77b9b0dd3145ca12a8ca57
adamgilg <adam.gilg@gmail.com>
2013-01-21 18:33:53 -0800

    Adam review

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -8,7 +8,8 @@ describe Deck do
 
   context "Deck" do
     its("cards.count") { should eq(52) }
-
+#We checked for shuffle with expect { deck.shuffle }.to change {deck.cards) 
+#on multiple lines. unsure if it works as a one liner, although it should
     it "should shuffle cards" do
       unshuffled = Deck.new
       deck.shuffle
@@ -16,6 +17,8 @@ describe Deck do
     end
 
     it "should allow cards to be taken off" do
+    #you're checking for two things here: for taking cards off
+    #and adding cards to the taken card count
       deck.take(2).count.should eq(2)
       deck.cards.count.should eq(50)
     end
@@ -30,7 +33,7 @@ describe Deck do
     it "should return cards to the bottom" do
       cards = deck.take(2)
       deck.return(cards)
-
+#doesn't really check if they're at the bottom
       deck.take(2).should_not eq(cards)
     end
   end
